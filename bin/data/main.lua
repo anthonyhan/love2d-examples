9464edea30c89faebf221a1bd8fb406328081e37
Anthony <qianhan1986@msn.com>
2015-07-28 15:46:39 +0800

    add draw hex edge

--- bin/data/main.lua
+++ bin/data/main.lua
@@ -1,28 +1,33 @@
+
+center = {}
+size = 40
+
 function love.load()
 	sound = love.audio.newSource("Boom_epic_win.ogg")
 	love.audio.play(sound)
 end
 
 function love.draw()
-	var center = {};
+	
 	center.x = 200;
 	center.y = 200;
-	var size = 20;
 	
-	var p1,p2;
+	love.graphics.clear()
+	love.graphics.setColor(255,255,255)
+	
 	for  i=1,6,1 
 	do
 		x1, y1 = hex_cornor(center, size, i-1)
-		p2 = hex_cornor(center, size, i)
-		draw_hex_edge(p1,p2)
+		x2, y2 = hex_cornor(center, size, i)
+		draw_hex_edge(x1, y1, x2, y2)
 	end
 
 end
 
 
 function hex_cornor(center, size, i)
-	var angle_deg = 60 * i
-	var angle_rad = PI/180 * angle_deg
+	local angle_deg = 60 * i
+	local angle_rad = math.pi/180 * angle_deg
 	return center.x + size * math.cos(angle_rad), center.y + size * math.sin(angle_rad)
 end
 
