04aa44c486b5a768993999f9d8ba00f2a0d0ab83
Anthony <qianhan1986@msn.com>
2015-07-30 18:10:27 +0800

    add neighbor display

--- src/main.lua
+++ src/main.lua
@@ -33,25 +33,34 @@ function love.draw()
 		for j=0,7,1
 		do
 			local hex_odd = hex(i,j)
-			hex_center = center_odd_q(hex_odd)
-			love.graphics.setColor(0,127, 127)
-			draw_hex(hex_center, HEX_SIZE)
-			love.graphics.setColor(255,255,255)
-			draw_hex_outline(hex_center, HEX_SIZE)
+			hex_center = get_hex_center(hex_odd)
+			draw_hex(hex_center, HEX_SIZE, color(0, 127, 127))
 		end	
 	end
-
-end
-
-
-function draw_hex_outline(center, size)
-	for  i=0,5,1 
+	
+	
+	--draw neighbor
+	hx1 = hex(6, 3) 
+	center_pt = get_hex_center(hx1)
+	draw_hex(center_pt, HEX_SIZE, color(127,0, 0))
+	
+	cb1 = oddq_to_cube(hx1)
+	for i=0,5,1
 	do
-		x1, y1 = hex_cornor(center, size, i)
-		x2, y2 = hex_cornor(center, size, i+1)
-		draw_hex_line(x1, y1, x2, y2)
+		cb_neighbor = cube_neighbor(cb1,i)
+		-- print("x=", cb_neighbor.x, "\ty=", cb_neighbor.y, "\tz =", cb_neighbor.z)
+		-- debug.debug()
+		hx_neighbor = cube_to_oddq(cb_neighbor)
+		if(hx_neighbor.q>=0 and hx_neighbor.q<=12 and hx_neighbor.r>=0 and hx_neighbor.r<=7)
+		then
+			neighbor_center_pt = get_hex_center(hx_neighbor)
+			draw_hex(neighbor_center_pt, HEX_SIZE, color(0, 127, 0))		
+		end
 	end
+	
+
 end
+
 
 function hex_cornor(center, size, i)
 	local angle_deg = 60 * i
@@ -59,16 +68,26 @@ function hex_cornor(center, size, i)
 	return center.x + size * math.cos(angle_rad), center.y + size * math.sin(angle_rad)
 end
 
-function draw_hex_line(x1, y1, x2, y2)
+function draw_hex_outline(x1, y1, x2, y2)
 	love.graphics.line(x1, y1, x2, y2)
 end
 
-function draw_hex(center, size)
+function draw_hex(center, size, color)
+	if(color == nil)
+	then
+		color = color(255,255,255)
+	end
+	
 	for i=0,5,1
 	do
-		x1, y1 = hex_cornor(center, size, i)
-		x2, y2 = hex_cornor(center, size, i+1)
+		local x1, y1 = hex_cornor(center, size, i)
+		local x2, y2 = hex_cornor(center, size, i+1)
+		
+		love.graphics.setColor(color.r, color.g, color.b, color.a)
 		draw_hex_polygon(center, point(x1,y1), point(x2, y2))
+		
+		love.graphics.setColor(255,255,255)
+		draw_hex_outline(x1, y1, x2, y2)
 	end
 end
 
@@ -80,13 +99,13 @@ end
 
 ----cube coordination caculation
 function cube_direction(dir)
-	direction = CUBE_DIRECTIONS[dir+1]
+	local direction = CUBE_DIRECTIONS[dir+1]
 	return cube(direction[1], direction[2], direction[3])
 end
 
-function cube_neighbor(hex, direction)
-	dir = cube_direction(direction)
-	return cube_add(hex_to_cube(hex), dir)
+function cube_neighbor(cube, direction)
+	local dir = cube_direction(direction)
+	return cube_add(cube, dir)
 end
 
 
@@ -97,7 +116,18 @@ function cube_add(cube1, cube2)
 end
 
 
-function center_odd_q(hex)
+function hex_direction(dir)
+	local direction = HEX_DIRECTIONS[dir+1]
+	return hex(direction[1], direction[2])
+end
+
+function hex_neighbor(hex1, direction)
+	local dir = hex_direction(direction)
+	return hex(hex1.q + dir.q, hex1.r + dir.r)
+end
+
+-- get center x,y of a hexagon. (odd-q type)
+function get_hex_center(hex)
 	x = hex.q * HEX_W * 0.75 + 0.5 * HEX_W
 	y = hex.r * HEX_H  + (hex.q%2)*0.5*HEX_H  + 0.5 * HEX_H
 	return point(x, y)
@@ -111,13 +141,13 @@ end
 
 
 ----coordination conversion
-function cube_to_hex(cx, cy, cz)
-	return cx, cz
-end
+-- function cube_to_hex(cx, cy, cz)
+	-- return cx, cz
+-- end
 
-function hex_to_cube(q, r)
-	return q, -x-z, z
-end
+-- function hex_to_cube(q, r)
+	-- return q, -x-z, z
+-- end
 
 function cube_to_oddq(cube)
 	return hex(cube.x,  cube.z + (cube.x - cube.x%2)/2)
@@ -125,7 +155,7 @@ end
 
 function oddq_to_cube(hex)
 	x = hex.q
-	z = hex.r - ( hex.r - hex.r%2)/2
+	z = hex.r - ( hex.q - hex.q%2)/2
 	y = -x-z
 	return cube(x, y, z)
 end
@@ -150,4 +180,13 @@ function point(x,y)
 	point.x = x
 	point.y = y
 	return point
+end
+
+function color(r, g, b, a)
+	local color = {};
+	color.r = r
+	color.g = g
+	color.b = b
+	color.a = a
+	return color
 end
