a6b32e59428af1ae25d812630a0e9cc0f916d7b9
Anthony <qianhan1986@msn.com>
2016-03-11 00:25:48 +0800

    delete resolution config

--- src/conf.lua
+++ src/conf.lua
@@ -1,10 +1,5 @@
 function love.conf(t)
-author = "anthony"
-love_version = "0.10.0"
-
-
-    t.window.width = 1024                -- The window width (number)
-    t.window.height = 800 
-	
+	author = "anthony"
+	love_version = "0.10.0"
 
 end
